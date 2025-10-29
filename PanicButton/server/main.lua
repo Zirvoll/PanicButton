69b3fdaf5150976aa70a5051705c16bbc091dcc0
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2025-10-29 11:55:26 +0100

    refactor(server/main): use GetPlayerInfo function from bridge files

--- PanicButton/server/main.lua
+++ PanicButton/server/main.lua
@@ -1,32 +1,30 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
 RegisterNetEvent("HapticPanic:TriggerButton", function()
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then return end
 
-    local job      = Player.PlayerData.job or {}
-    local charinfo = Player.PlayerData.charinfo or {}
+    if not FrameworkFound then
+        error(string.format("Script has not found your framework, please make sure it exists within: '@%s/server/bridges/'", GetCurrentResourceName()))
+    end
 
     local ped = GetPlayerPed(src)
+    local Player = GetPlayerInfo(src)
+
     local x, y, z = table.unpack(GetEntityCoords(ped))
     TriggerClientEvent("InteractSound_CL:PlayWithinDistance", -1, src, 30.0, "panic", 0.2)
 
-    local dept = tostring(job.name or "")
+    local dept = tostring(Player.job or "")
     local label = ("X: %.2f, Y: %.2f"):format(x, y)
 
     local dispatch = {
         priority    = "high",
         code        = "Panic Alert",
         title       = "Panic Button Activated",
-        description = (("%s %s has triggered their panic button!"):format(
-            tostring(charinfo.firstname or ""), tostring(charinfo.lastname or "")
-        )),
-        location    = { label = label, coords = { x = x + 0.0, y = y + 0.0 } },
+        description = (("%s has triggered their panic button!"):format(Player.name)),
+        location    = { label = label, coords = { x = x, y = y } },
         time        = 180,
         sound       = "panic-button.mp3",
         fields      = { { icon = "fa-solid fa-shield-halved", label = "Department", value = dept:upper() } },
     }
+
     if dept == "police" or dept == "sast" or dept == "sheriff" or dept == "lspd" then
         dispatch.job = "police"
     elseif dept == "ambulance" or dept == "ems" then
