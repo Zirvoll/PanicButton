b2f1e5d0011302dbd8103556872d88d9e8d5181a
Phantom <161694969+Zirvoll@users.noreply.github.com>
2025-11-29 02:44:03 -0600

    Implement job-based access for panic button
    Added job check for panic button access and notification.
    
    - Thank you for the code logic changes MadddDat!

--- PanicButton/client/main.lua
+++ PanicButton/client/main.lua
@@ -1,8 +1,22 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
 local function TriggerPanic()
     TriggerServerEvent("HapticPanic:TriggerButton")
 end
+
 RegisterCommand("panic", function()
-    TriggerPanic()
+    local playerData = QBCore.Functions.GetPlayerData()
+    local job = playerData.job.name
+
+    if job == "police" or job == "sast" or job == "ambulance" then
+        TriggerPanic()
+    else
+        lib.notify({
+            title = 'Access Denied',
+            description = 'You are not authorized to use the panic button.',
+            type = 'error'
+        })
+    end
 end, false)
 
 RegisterKeyMapping("panic", "Trigger Panic Button", "keyboard", "F11")
