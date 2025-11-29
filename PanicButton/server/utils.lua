4f26fd53875a9063c11fe59efc3748136fcd88ca
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2025-11-29 13:41:34 +0100

    feat(server/utils): ArrayContains helper function

--- PanicButton/server/utils.lua
+++ PanicButton/server/utils.lua
@@ -2,3 +2,15 @@ function IsResourceActive(resource)
     local state = GetResourceState(resource)
     return state == 'started' or state == 'starting'
 end
+
+---Check if an item is in an array
+---@param array table
+---@param item number | string
+---@return boolean
+function ArrayContains(array, item)
+    for i = 1, #array do
+        if array[i] == item then return true end
+    end
+
+    return false
+end
